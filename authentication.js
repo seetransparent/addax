784d9524a17963090dfc5aea9059d52e4e4fc040
Elias A. G.-C <eliasagc@gmail.com>
2017-12-14 17:08:06 +0100

    fix(authentication): do not crash if auth.json does not exist yet

--- authentication.js
+++ authentication.js
@@ -2,10 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 function readAuthFile() {
+  const authFile = path.join(__dirname, 'auth.json');
   return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, 'auth.json'), (err, file) => {
-      if (err) return reject(err);
-      resolve(JSON.parse(file));
+    fs.access(authFile, fs.constants.R_OK, (err) => {
+      // if the file does not exist, read it as an empty object
+      if (err) return resolve({});
+      fs.readFile(authFile, (err, file) => {
+        if (err) return reject(err);
+        resolve(JSON.parse(file));
+      });
     });
   });
 }
